207804ea91b17e940cedd1cb6d3d0812abee9a47
Deep101086 <60017248+Deep101086@users.noreply.github.com>
2020-07-01 18:59:40 -0700

    Complete

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-  $("#hide").click(function(){
-    $("p").hide();
-  });
-  $("#show").click(function(){
-    $("p").show();
-  });
-});
-</script>
+  $( function() {$( "#tabs" ).tabs(); } );  
+  
+  //Get the button
+  var mybutton = document.getElementById("myBtn");    
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
